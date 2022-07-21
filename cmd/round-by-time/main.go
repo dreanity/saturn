d025eda2e55fbdd2f509158414c839f64cf9f8c6
AlexTroshkin <alextroshkin@outlook.com>
2022-07-21 18:20:07 +0000

    randomness: add keeper fn SetUnprovenRandomnessForTime

---
+++ cmd/round-by-time/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func main() {
+	round := roundAt(1658425688)
+	fmt.Println("Round #", round)
+}
+
+func roundAt(time uint64) uint64 {
+	genesisTime := uint64(1595431050)
+	preiod := uint64(30)
+	round := uint64(1)
+
+	if time < genesisTime {
+		round = 1
+	} else {
+
+		timeDiff := sdk.NewDecFromBigInt(sdk.NewIntFromUint64(time - genesisTime).BigInt())
+		roundForTime := timeDiff.QuoRoundUp(sdk.NewDecFromInt(sdk.NewIntFromUint64(preiod)))
+		nextRound := roundForTime.Ceil().TruncateInt().Uint64() + 1
+
+		println("TimeDiff uint64   : ", time-genesisTime)
+		println("TimeDiff sdk.Dec  : ", sdk.NewDecFromBigInt(sdk.NewIntFromUint64(time-genesisTime).BigInt()).String())
+		println("TimeDiff / Period : ", timeDiff.QuoRoundUp(sdk.NewDecFromInt(sdk.NewIntFromUint64(preiod))).String())
+		println("Next round        : ", roundForTime.Ceil().TruncateInt().Uint64()+1)
+
+		round = nextRound
+	}
+
+	return round
+}
