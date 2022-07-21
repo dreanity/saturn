8103eb55ab7f3b4c485e9ff80757504ff003823d
AlexTroshkin <alextroshkin@outlook.com>
2022-07-21 18:30:10 +0000

    randomness: add keeper fn SetUnprovenRandomnessForTime

--- x/randomness/keeper/unproven_rendomness.go
+++ x/randomness/keeper/unproven_rendomness.go
@@ -2,36 +2,33 @@ package keeper
 
 import (
 	"saturn/x/randomness/types"
+	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k Keeper) SetUnprovenRandomnessForTime(ctx sdk.Context, time uint64) {
-	// chainInfo := k.GetChainInfo(ctx)
-	// round := uint64(1)
+	chainInfo := k.GetChainInfo(ctx)
+	round := uint64(1)
 
-	// if time < chainInfo.GenesisTime {
-	// 	round = 1
-	// } else {
-	// 	timeDiff := sdk.NewDecFromBigInt(sdk.NewIntFromUint64(time - chainInfo.GenesisTime).BigInt())
-	// 	roundForTime := timeDiff.QuoRoundUp(sdk.NewDecFromInt(sdk.NewIntFromUint64(chainInfo.GenesisTime)))
-	// 	nextRound := roundForTime.BigInt().Uint64() + 1
+	if time < chainInfo.GenesisTime {
+		round = 1
+	} else {
+		timeDiff := sdk.NewDecFromBigInt(sdk.NewIntFromUint64(time - chainInfo.GenesisTime).BigInt())
+		roundForTime := timeDiff.QuoRoundUp(sdk.NewDecFromInt(sdk.NewIntFromUint64(chainInfo.Period)))
+		nextRound := roundForTime.Ceil().TruncateInt().Uint64() + 1
 
-	// 	round = nextRound
-	// }
+		round = nextRound
+	}
 
-	// unprovenRandomness := types.UnprovenRandomness{
-	// 	Index: string(time),
-	// 	Round: round,
-	// 	Time: time,
-	// }
+	unprovenRandomness := types.UnprovenRandomness{
+		Index: strconv.FormatUint(time, 10),
+		Round: round,
+		Time:  time,
+	}
 
-	// store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.UnprovenRandomnessKeyPrefix))
-	// b := k.cdc.MustMarshal(&unprovenRandomness)
-	// store.Set(types.UnprovenRandomnessKey(
-	// 	unprovenRandomness.Index,
-	// ), b)
+	k.SetUnprovenRandomness(ctx, unprovenRandomness)
 }
 
 // SetUnprovenRandomness set a specific unprovenRandomness in the store from its index
