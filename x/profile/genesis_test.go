1b4b970956e523d54f4936427c6515898e016e43
AlexTroshkin <alextroshkin@outlook.com>
2022-10-01 23:11:42 +0000

    profile: scaffold module

---
+++ x/profile/genesis_test.go
@@ -0,0 +1,29 @@
+package profile_test
+
+import (
+	"testing"
+
+	keepertest "github.com/dreanity/saturn/testutil/keeper"
+	"github.com/dreanity/saturn/testutil/nullify"
+	"github.com/dreanity/saturn/x/profile"
+	"github.com/dreanity/saturn/x/profile/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.ProfileKeeper(t)
+	profile.InitGenesis(ctx, *k, genesisState)
+	got := profile.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
